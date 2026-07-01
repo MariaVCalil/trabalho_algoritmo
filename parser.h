e4690876f6343e6b3bdfa6d9a49432c49d1ee0ee
mvc <calil.vitoria@estudantes.ifg.edu.br>
2026-07-01 06:55:54 -0300

    alteração parser.h

--- parser.h
+++ parser.h
@@ -4,12 +4,16 @@
 #include "config.h"
 #include "matriz.h"
 
+/* Representa uma variavel encontrada na equacao */
+
 typedef struct
 {
     char nome;
     int coluna;
 
 } Variavel;
+
+/* Guarda todas as variaveis do sistema */
 
 typedef struct
 {
@@ -18,17 +22,17 @@ typedef struct
 
 } Variaveis;
 
+/* Guarda uma equacao ja interpretada (só numeros) */
 
 typedef struct
 {
     double coeficientes[MAX_VARIAVEIS];
     double resultado;
-
     int valida;
 
 } Equacao;
 
-
+/* Guarda as equacoes digitadas pelo usuario */
 
 typedef struct
 {
@@ -37,17 +41,11 @@ typedef struct
 
 } SistemaEntrada;
 
-
-
 void lerSistema(SistemaEntrada *entrada);
 
+int identificarVariaveis(const SistemaEntrada *entrada, Variaveis *vars);
 
-int identificarVariaveis(const SistemaEntrada *entrada,Variaveis *vars);
-
-
-
-int montarSistema(const SistemaEntrada *entrada,const Variaveis *vars, Matriz *matriz);
-
+int montarSistema(const SistemaEntrada *entrada, const Variaveis *vars, Matriz *matriz);
 
 void imprimirVariaveis(const Variaveis *vars);
 
