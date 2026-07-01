276d2040fe17835cd8782e05731d3b1071313a55
mvc <calil.vitoria@estudantes.ifg.edu.br>
2026-07-01 11:00:18 -0300

    alteração base.h

--- base.h
+++ base.h
@@ -4,15 +4,11 @@
 #include "matriz.h"
 
 /*
-==========================================================
 Questao 3 - Bases em R2 e R3
-
-O programa:
     - le um conjunto de vetores
     - identifica automaticamente se esta em R2 ou R3
     - verifica se forma base
-    - se nao formar, indica subconjunto ou completacao
-==========================================================
+    - se nao formar, indica subconjunto/completacao
 */
 
 void executarBases(void);
