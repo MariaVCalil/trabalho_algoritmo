7559d62fdf4c9b260220f8fe19dd713e4983541a
mvc <calil.vitoria@estudantes.ifg.edu.br>
2026-07-01 16:01:38 -0300

    alteração autovalore.h

--- autovalores.h
+++ autovalores.h
@@ -3,33 +3,20 @@
 
 #include "matriz.h"
 
-/*
-==========================================================
-Questao 4 - Autovalores e Autovetores em R2
-
-O programa:
+/* Questao 4 - Autovalores e autovetores em R2 
     - le um operador T: R2 -> R2
     - monta a matriz associada
     - calcula autovalores reais
     - calcula autovetores
     - informa quando nao houver autovalores reais
-==========================================================
 */
 
 void executarAutovalores(void);
 
-int calcularAutovalores2x2(const Matriz *matriz,
-                           double autovalores[2],
-                           int multiplicidades[2],
-                           int *quantidade);
+int calcularAutovalores2x2(const Matriz *matriz, double autovalores[2], int multiplicidades[2], int *quantidade);
 
-void calcularAutovetor2x2(const Matriz *matriz,
-                          double lambda,
-                          double vetor[2]);
+void calcularAutovetor2x2(const Matriz *matriz, double lambda, double vetor[2]);
 
-int montarMatrizOperadorR2(const char texto[],
-                           Matriz *matriz,
-                           char variaveis[2]);
-                                                 
+int montarMatrizOperadorR2(const char texto[], Matriz *matriz, char variaveis[2]);
 
 #endif
